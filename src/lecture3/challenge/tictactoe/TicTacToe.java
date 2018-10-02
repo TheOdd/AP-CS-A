0c18d205848c6def585e4727365f17e694b9afad
TheOdd <owenhines07@gmail.com>
2018-10-02 10:29:53 -0500

    Moved run loop to its own method.

--- src/lecture3/challenge/tictactoe/TicTacToe.java
+++ src/lecture3/challenge/tictactoe/TicTacToe.java
@@ -15,49 +15,63 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class TicTacToe {
 	public static void main(String[] args) {
+		/* The reason we have the Scanner outside the run method is because we can't close it after
+		 * every run() call. This is caused by the fact that the close() method also actually
+		 * closes the System.in stream as well as the Scanner stream. And, from what I can tell,
+		 * you can't re-open the System.in stream after it has been closed.
+		 */
+		
 		Scanner input = new Scanner(System.in);
 		
+		// Run game as many times as user wants
+		while (run(input));
+		
+		System.out.println("Thanks for playing!");
+		
+		input.close();
+	}
+
+	// Returns true/false based on if player wants to play again
+	private static boolean run(Scanner input) {
+
 		// Initialize board with reference numbers
 		String[][] b = {{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
-		
+
 		printBoard(b);
 		System.out.println("You are playing as X.");
-		
+
 		int choice;
 		int moveCount = 0;
-		
+
 		while (getWinner(b) == null) {
 			do {
 				System.out.println("Please select a move.");
 				choice = input.nextInt();
 			} while (choice < 1 || choice > 9 || makeMove(b, choice, "X") == null);
-			
+
 			b = makeMove(b, choice, "X");
-			
+
 			moveCount++;
-			
+
 			if (moveCount >= 5 || getWinner(b) != null)
 				break;
 			b = moveAI(b);
-			
+
 			printBoard(b);
 		}
-		
+
 		if (moveCount == 5)
 			System.out.println("Tie!");
 		else
 			System.out.println(getWinner(b) + " wins!");
-		
+
 		System.out.println("Would you like to play again? (Y/N)");
 
 		boolean playAgain = input.next().equalsIgnoreCase("Y");
-
-		input.close();
 		
-		if (playAgain)
-			main(null);
+		return playAgain;
 	}
-	
+
 	private static void printBoard(String[][] board) {
 		System.out.println(" ––– ––– –––");
 		for (String[] row : board) {
@@ -67,7 +81,7 @@ public class TicTacToe {
 			System.out.print("|" + '\n' + " ––– ––– –––" + '\n');
 		}
 	}
-	
+
 	private static String[][] makeMove(String[][] board, int move, String player) {
 		String[][] boardClone = cloneBoard(board);
 		String spotVal = board[(move - 1) / 3][(move - 1) % 3];
@@ -77,14 +91,14 @@ public class TicTacToe {
 		} else
 			return null;
 	}
-	
+
 	// Computer chooses random tile to play
 	private static String[][] moveAI(String[][] board) {
 		int move = ThreadLocalRandom.current().nextInt(1, 10);
 		for (; makeMove(board, move, "O") == null; move = ThreadLocalRandom.current().nextInt(1, 10)) {}
 		return makeMove(board, move, "O");
 	}
-	
+
 	private static String[][] cloneBoard(String[][] board) {
 		String[][] boardClone = board.clone();
 		for (int i = 0; i < boardClone.length; i++) {
@@ -94,7 +108,7 @@ public class TicTacToe {
 		}
 		return boardClone;
 	}
-	
+
 	private static String getWinner(String[][] board) {
 		// Check win conditions
 		// Check rows
@@ -102,19 +116,19 @@ public class TicTacToe {
 			if (row[0].equals(row[1]) && row[1].equals(row[2]))
 				return row[0];
 		}
-		
+
 		// Check columns
 		for (int col = 0; col < 3; col++) {
 			if (board[0][col].equals(board[1][col]) && board[1][col].equals(board[2][col]))
 				return board[0][col];
 		}
-		
+
 		// Check diagonals
 		if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]))
 			return board[0][0];
 		if (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]))
 			return board[1][1];
-		
+
 		return null;
 	}
 }
