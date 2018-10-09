da44ee6c30e207c7916fd610bcb5acaea5c4d86c
TheOdd <owenhines07@gmail.com>
2018-10-09 11:42:13 -0500

    Added working Game of Life exercise
    The code is a bit messy and poorly commented. I'm also not too confident in the way I structured this solution. I may refactor it later.

---
+++ src/lecture8/optional/gameoflife/Board.java
@@ -0,0 +1,89 @@
+package lecture8.optional.gameoflife;
+
+public class Board {
+	private int size;
+	private int[][] matrix;
+	public Board(int s) {
+		this.setSize(s);
+		this.generateMatrix();
+	}
+	
+	private void setSize(int size) {
+		this.size = size;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	private void generateMatrix() {
+		matrix = new int[size][size];
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				matrix[row][col] = (int)Math.floor(Math.random() + 0.5);
+			}
+		}
+	}
+	
+	private int getNeighbors(int row, int col) {
+		int neighbors = 0;
+		
+		for (int r = row - 1; r < (row + 2); r++) {
+			for (int c = col - 1; c < (col + 2); c++) {
+				if (r == row && c == col)
+					continue;
+				int point = getPoint(r, c);
+				if (point == 2)
+					continue;
+				neighbors += point;
+			}
+		}
+		
+		return neighbors;
+	}
+	
+	private int getPoint(int row, int col) {
+		try {
+			return matrix[row][col];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return 2;
+		}
+	}
+	
+	private int getUpdatedPoint(int row, int col) {
+		int state = getPoint(row, col);
+		int neighbors = getNeighbors(row, col);
+		
+		if (state == 0) { // dead
+			return (neighbors == 3) ? 1 : 0;
+		} else { // live
+			return (neighbors == 1 || neighbors > 3) ? 0 : 1;
+		}
+	}
+	
+	public void update() {
+		int[][] tempMatrix = new int[size][size];
+		
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				tempMatrix[row][col] = getUpdatedPoint(row, col);
+			}
+		}
+		
+		matrix = tempMatrix;
+	}
+	
+	public String toString() {
+		String s = "";
+		
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				s += getPoint(row, col);
+				s += " ";
+			}
+			s += "\n";
+		}
+		
+		return s;
+	}
+}
---
+++ src/lecture8/optional/gameoflife/GameOfLife.java
@@ -0,0 +1,43 @@
+package lecture8.optional.gameoflife;
+
+import java.util.Scanner;
+
+/*
+ * Optional Exercise
+ * 
+ * Try implementing Game of Life on a boolean matrix from section 2.4.20
+ * 
+ * Rules:
+ * 
+ * A dead cell with exactly three live neighbors becomes live.
+ * A live cell with exactly one live neighbor becomes dead.
+ * A live cell with more than three live neighbors becomes dead.
+ */
+
+public class GameOfLife {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please input the size of the board (n x n)");
+		Board b = new Board(input.nextInt());
+		
+		System.out.println("Please input the number of generations to go through.");
+		int t = input.nextInt();
+		
+		System.out.print("\n");
+		
+		input.nextLine();
+		
+		for (int current = 1; current <= t; current++) {
+			System.out.println("Generation " + current + "\n");
+			System.out.print(b);
+			for (int i = 0; i < (2 * b.getSize() - 1); i++)
+				System.out.print('–');
+			System.out.println("\n");
+			b.update();
+			input.nextLine();
+		}
+		
+		input.close();
+	}
+}
