8a7bd795cbeb241eb8f16dc334a53a812ff85547
TheOdd <owenhines07@gmail.com>
2018-11-07 16:46:48 -0600

    Added arrayToLinkedList helper method. Implemented add method. Made output more clear.

--- src/lecture16/standard/linkedlist/LinkedListTools.java
+++ src/lecture16/standard/linkedlist/LinkedListTools.java
@@ -42,4 +42,70 @@ public abstract class LinkedListTools {
 		
 		return head;
 	}
+	
+	public static <T> LinkedListNode<T> arrayToLinkedList(T[] arr) {
+		if (arr.length == 0)
+			return null;
+		
+		LinkedListNode<T> head = new LinkedListNode<T>(arr[0]);
+		LinkedListNode<T> pointer = head;
+		
+		for (int i = 1; i < arr.length; i++) {
+			pointer.next = new LinkedListNode<T>(arr[i]);
+			pointer = pointer.next;
+		}
+		
+		return head;
+	}
+	
+	public static LinkedListNode<Integer> add(LinkedListNode<Integer> head1, LinkedListNode<Integer> head2) {
+		if (head1 == null || head2 == null)
+			return null;
+		
+		if (head1.length() < head2.length()) {
+			LinkedListNode<Integer> temp = head1;
+			head1 = head2;
+			head2 = temp;
+		}
+		
+		head1 = reverse(head1);
+		head2 = reverse(head2);
+		
+		LinkedListNode<Integer> result = head1;
+		LinkedListNode<Integer> tail1 = null;
+		
+		int carry = 0;
+		int added;
+		
+		do {
+			added = (head1.val + head2.val + carry) % 10;
+			carry = (head1.val + head2.val + carry) / 10;
+			
+			head1.val = added;
+			
+			if (head1.next == null)
+				tail1 = head1;
+			
+			head1 = head1.next;
+			head2 = head2.next;
+		} while (head2 != null);
+		
+		while (carry != 0) {
+			if (head1 == null) {
+				tail1.next = new LinkedListNode<Integer>(0);
+				head1 = tail1.next; 
+			}
+			
+			added = (head1.val + carry) % 10;
+			carry = (head1.val + carry) / 10;
+			head1.val = added;
+			
+			if (carry != 0) {
+				head1.next = new LinkedListNode<Integer>(0);
+				head1 = head1.next;
+			}
+		}
+		
+		return reverse(result);
+	}
 }
--- src/lecture16/standard/linkedlist/Main.java
+++ src/lecture16/standard/linkedlist/Main.java
@@ -9,27 +9,39 @@ public class Main {
 		int n;
 		
 		do {
+			System.out.println("Please input any positive integer.");
 			n = input.nextInt();
 		} while (n <= 0);
 		
 		input.close();
 		
 		LinkedListNode<Integer> list = new LinkedListNode<Integer>((int)(Math.random() * 10));
-		LinkedListNode<Integer> listCopy = list;
+		LinkedListNode<Integer> pointer = list;
 		
 		for (int i = 1; i < n; i++) {
 			// Random int 0-9
 			int random = (int)(Math.random() * 10);
 			
-			listCopy.next = new LinkedListNode<Integer>(random);
-			listCopy = listCopy.next;
+			pointer.next = new LinkedListNode<Integer>(random);
+			pointer = pointer.next;
 		}
 		
 		// list is now the head of an n-long LinkedList, with each node containing a random integer from 0-9.
-		// listCopy is no longer needed, as it just references the tail node of list
+		// pointer is no longer needed, as it just references the tail node of list
 		
-		System.out.println(list);
+		System.out.println("Randomly generated list: " + list);
 		
-		System.out.println(LinkedListTools.reverse(list));
+		System.out.println("Reversed list: " + LinkedListTools.reverse(list));
+		
+		Integer[] arr1 = {2, 9, 7, 4, 3, 8, 2, 9, 7, 3, 8, 6, 3, 2, 1, 3, 5};
+		Integer[] arr2 = {2, 8, 1, 4, 3, 2, 9, 3, 4, 8, 1, 3, 5, 2};
+		
+		LinkedListNode<Integer> head1 = LinkedListTools.arrayToLinkedList(arr1);
+		LinkedListNode<Integer> head2 = LinkedListTools.arrayToLinkedList(arr2);
+		
+		System.out.println("First number in addition operation: " + head1);
+		System.out.println("Second number in addition operation: " + head2);
+		
+		System.out.println("Sum: " + LinkedListTools.add(head1, head2));
 	}
 }
